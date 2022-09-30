4e9eeb44adb69d110c5a77f8612cac01476e3242
Najam Rauf <najamsaqibr@nycstudents.net>
2022-09-30 12:32:05 -0400

    edited code

--- src/RectangleRunner.java
+++ src/RectangleRunner.java
@@ -1,7 +1,25 @@
+import java.util.Scanner;
+
 public class RectangleRunner {
     public static void main(String[] args) {
-        Rectangle rect1 = new Rectangle(10, 20);
-        int area = rect1.calculateArea();
-        System.out.println("This rectangle has an area of " + area + " and a volume of " + rect1.calculateBoxVolume(25));
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter rectangle 1 length: ");
+        int rect1Length = myScanner.nextInt();
+        System.out.print("Enter rectangle 1 width: ");
+        int rect1Width = myScanner.nextInt();
+        System.out.print("Enter rectangle 1 height: ");
+        double rect1Height = myScanner.nextDouble();
+        System.out.print("Enter rectangle 2 length: ");
+        int rect2Length = myScanner.nextInt();
+        System.out.print("Enter rectangle 2 width: ");
+        int rect2Width = myScanner.nextInt();
+        System.out.print("Enter rectangle 2 height: ");
+        double rect2Height = myScanner.nextDouble();
+
+        // write the rest of your program below
+        Rectangle rect1 = new Rectangle(rect1Length, rect1Width);
+        Rectangle rect2 = new Rectangle(rect2Length, rect2Width);
+        System.out.println("Rectangle 1's Area")
     }
 }
+
